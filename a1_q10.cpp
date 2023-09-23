365a177313a13044dde0b1240ffe3f184e05e69c
Kashish Rastogi <142674246+kashishrastogi1@users.noreply.github.com>
2023-09-23 21:20:55 +0530

    ans 10 of assignment1

---
+++ a1_q10.cpp
@@ -0,0 +1,16 @@
+// prime factors
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int div=2;div<n;div++){
+        while (n%div==0){
+            n/=div;
+            cout<<div<<" ";
+        }
+    }
+    if(n!=1){
+        cout<<n<<endl;
+    } 
+}
