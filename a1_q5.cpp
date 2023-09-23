b084c8887f15bf0a81c3cd496ac0d199ddf3bbe2
Kashish Rastogi <142674246+kashishrastogi1@users.noreply.github.com>
2023-09-23 11:04:06 +0530

    ans 5 of assignment1

---
+++ a1_q5.cpp
@@ -0,0 +1,15 @@
+// count digits in number
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int digits=0;
+    while(n>0){
+        digits++;
+        n=n/10;
+    }
+    cout<<digits;
+    return 0;
+
+}
